76c1e7fcae1d03923d4524f9663c1b58a65d2443
melizka <limelnik0509@gmail.com>
2025-01-20 21:11:56 +0200

    update name files

---
+++ LogicCase.java
@@ -0,0 +1,21 @@
+class Car {
+    String brand;
+    int year;
+
+    // Конструктор
+    public Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+}
+
+public class LogicCase {
+    public static void main(String[] args) {
+
+        String car1 = new String("toyota");
+        String car2 = new String("toyota");
+
+        System.out.println("Результат ==: " + (car1 == car2));
+        System.out.println("Результат equals(): " + car1.equals(car2));
+    }
+}
